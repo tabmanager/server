eaf15cbe08a287aa3df71bea9a9c0fc95d87518f
Rahul Desai <rd825@georgetown.edu>
2019-03-11 18:55:33 -0400

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,24 @@ module.exports = {
 
   },
 
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/auth.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+
+  },
+
 };
